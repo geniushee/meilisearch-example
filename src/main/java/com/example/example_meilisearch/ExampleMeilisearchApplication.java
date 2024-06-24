15c3609662f87198e2ff92fd1a63bf003b7531a7
geniushee <h1116jeon@gmail.com>
2024-06-24 12:48:29 +0900

    git init

---
+++ src/main/java/com/example/example_meilisearch/ExampleMeilisearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.example_meilisearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleMeilisearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleMeilisearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/example_meilisearch/ExampleMeilisearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example_meilisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMeilisearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
